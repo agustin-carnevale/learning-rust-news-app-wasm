762ded713ca641f72ced553b6e6fd266b8584a4f
Agustin Carnevale <agustinc.dev@gmail.com>
2022-05-09 15:55:13 -0300

    initial commit

---
+++ headlines/src/main.rs
@@ -0,0 +1,11 @@
+use eframe::{NativeOptions, egui::Vec2, run_native};
+use headlines::Headlines;
+
+fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Headlines::new();
+    let mut win_options = NativeOptions::default();
+    win_options.initial_window_size = Some(Vec2::new(640.0, 860.0));
+    run_native(Box::new(app), win_options);
+}
